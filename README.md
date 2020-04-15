40d7f419ac2ccebb492ff54cf06b584f8407ab90
Jasper Ristkok <jasper.ristkok@ut.ee>
2020-04-15 22:00:39 +0300

    Update to v1.3

--- README.md
+++ README.md
@@ -9,13 +9,14 @@ If you want to read the code, I advise to read Fityk's [manual](https://fityk.ni
 
 There are 2 scripts.
 
-1. "analyze.lua" is foolproof and should work with cfityk.
-2. "analyze_and_plot.lua" is meant to be used with the GUI version. I can't guarantee that the script will work for all different computers because I'm using GUI through script, athough plotting with scripts isn't fully supported.
+1. "analyze.lua" is foolproof and should work with cfityk. However, I primarily use the next one so analyze.lua might be behind in features.
+2. "analyze_and_plot.lua" is meant to be used with the GUI version. I can't guarantee that the script will work for all different computers because I'm using GUI through script, athough plotting with scripts isn't (fully) supported.
 
 The script is written for spectra files gained from SOLIS software using Andor iStar340T ICCD camera. The files are generic columnar files where 1st column is pixel value and all other columns are different experiments.
 
-Also the GUI is unresponsive while Fityk is calculating stuff. That's why you won't see progress in the command line output. Although you can see how far the process is by checking the output file or drawn images. Depending on your computer and how many datapoints and functions you have, the process might take a long time. For me (Macbook pro 3,1 GHz Intel Core i5) it took 16 seconds to analyze and draw 16 spectra in the example folder.
+Also the GUI is unresponsive while Fityk is calculating stuff. That's why you won't see progress in the command line output. Although you can see how far the process is by checking the output file or drawn images. Depending on your computer and how many datapoints and functions you have, the process might take a long time. 
 
+For me (Macbook pro 3,1 GHz Intel Core i5, using 1 instance of Fityk (a.k.a 25% of CPU power)) it took 16 seconds to analyze and draw 16 spectra in the example folder. Using analyze_and_plot script v1.3 with 36 Voigt functions and a constant, the script processed 72 experiments in 84 minutes. The processing time probably scales exponentially with the number of functions.
 ---
 
 ## Example folder
@@ -27,9 +28,34 @@ I added an example folder in which there are 2 spectra series and an example inf
 ## Using the script
 
 1. Download and install Fityk
-2. Put the spectra files and the info file in an input folder.
+2. Put the spectra files, info file and stopscript.txt in an input folder.
 	* The spectra files have to be named as subsequent integers (e.g. as in the example 37, 38, 39...)
 	* Info file has to have 5 columns: file number, exposure time, number of accumulations, gain and gate width. If these parameters do not apply for you, write them as 1-s (except for gain, write gain as 0). The file has to be .txt file.
-3. Edit the script through Fityk or with a text editor. Change the constants at the beginning.
+	* stopcscript.txt is for stopping the script without quitting Fityk. If the file has any content, script will exit the processing loop. If you write something in stopscript.txt you also have to save the file for changes to be applied.
+3. Edit the script through Fityk or with a text editor. Change the constants at the beginning. You might have to try out multiple different constant values so that the script will work for your application. It is advisable to list the lines in the order where the most intense peak is the first. The script creates funtions in the order of the list and this way intense and weak peaks don't accidentaly swich places. 
 4. Run Fityk and use the script.
-	* If you want to plot and draw images from the graphs, you have to use "analyze_and_plot.lua". To change the look of drawn .png files you have to manually import a spectra to Fityk (in the GUI) and add the number of functions you want to have. Then you have to modify the look of everything (like colours) except the ranges of the plot. Finally make sure you click on dataset @0 so that it is selected (highlighted) and then run the script. 
+	* If you want to plot and draw images from the graphs, you have to use "analyze_and_plot.lua". To change the look of drawn .png files you have to manually import a spectra to Fityk (in the GUI) and add the number of functions you want to have. plot_helper.lua takes care of that automatically. Then you have to modify the look of everything (like colours) except the ranges of the plot. Finally make sure you click on dataset @0 so that it is selected (highlighted) and then run the analyzing script. 
+
+---
+
+## Tips and tricks
+
+* plot_helper.lua plots 40 gaussian functions and a constant. You can colour them as you like the output to be because running analyze_and_plot after that will keep the colours.
+* If you started the script and it won't stop for a long time and you don't want to quit the program since then you would lose the colourcode, you can write something in the stopscript.txt in the input folder. The script reads the file and if the file isn't empty then the script will exit the loop. Note that when you write something in the file you also have to save the file. Otherwise changes won't be applied.
+* Fityk uses only 1 core thread (at least the LUA part). If you want to squeeze more juice out of your computer, run multiple instances of Fityk and the script in different regions of experiment numbers effectively using multithreading and multiple cores manually. There isn't a native LUA workaround since LUA doesn't support hardware multithreading. However, this way you have to apply the colours to every instance again.
+* Example colour scheme: (nr. of function. colour)
+	0. snow
+	1. maraschino
+	2. flora
+	3. aqua
+	4. strawberry
+	5. tangerine
+	6. clover
+	7. ice
+	8. carnation
+	9. maraschino
+	10. flora
+	11. aqua
+	12. strawberry
+	13. ...
+* Once the script took 5 minutes to process 1 experiment with 37 functions. When I quit Fityk and reopened it everything was about 5 times quicker. So if you think the script works absurdly slow, try to quit Fityk and reopen it (might have been something with RAM usage).
--- analyze_and_plot.lua
+++ analyze_and_plot.lua
@@ -1,5 +1,5 @@
 -- Lua script for Fityk GUI version.
--- Script version: 1.2
+-- Script version: 1.3
 -- Author: Jasper Ristkok
 
 --[[
@@ -29,132 +29,238 @@ way you want it to look, click on the dataset @0 and then run the script.
 -- Change constants below
 ----------------------------------------------------------------------
 -- Constants, change them
+
 -- How many spectra lines are there?
-nr_of_lines=21
+nr_of_lines=37
+
 -- Where are they in pixels? (only the first nr_of_lines lines are used)
 -- By convention in lua, thhe first index is 1 instead of 0
-line_positions={
-973,    --1
-1314,   --2
-1504,   --3
-483,    --4
-510,    --5
-553,    --6
-639,    --7
-651,    --8
-714,    --9
-866,    --10
-913,    --11
-1046,   --12
-1105,   --13
-1156,   --14
-1253,   --15
-1267,   --16
-1355,   --17
-1451,   --18
-1553,   --19
-1595,   --20
-1634}   --21
+line_positions=
+{
+-- Most intense peaks
+612,    --1
+980,    --2
+1198,   --3
+-- Other intense peaks
+522,    --4
+546,    --5
+783,    --6
+810,    --7
+941,    --8
+1022,   --9
+1229,   --10
+1268,   --11
+1326,   --12
+1413,   --13
+1422,   --14
+1526,   --15
+1564,   --16
+-- Smaller intensity or mathematical peaks
+457,    --17
+509,    --18
+528,    --19
+633,    --20
+655,    --21
+689,    --22
+733,    --23
+751,    --24
+771,    --25
+802,    --26
+819,    --27
+891,    --28
+965,    --29
+1008,   --30
+1031,   --31
+1090,   --32
+1108,   --33
+1280,   --34
+1354,   --35
+1479,   --36
+1595    --37
+}
+
 -- How far can the peak shift? If array element is non-0, 
 -- then bigger shifts are considered lack of peak
 -- Writing the value of the corresponding peak as -1 uses the default 30% domain.
 -- 0 locks the line in place
-line_center_domains={
-40,     --1
-3,      --2
-3,      --3
-3,      --4
-6,      --5
-3,      --6
-3,      --7
-3,      --8
-3,      --9
-3,      --10
-3,      --11
-3,      --12
-3,      --13
-3,      --14
-3,      --15
-3,      --16
-3,      --17
-3,      --18
-30,     --19
-3,      --20
-3}      --21
+line_center_domains=
+{
+3,	--1
+3,	--2
+3,	--3
+3,	--4
+3,	--5
+3,	--6
+3,	--7
+3,	--8
+3,	--9
+3,	--10
+3,	--11
+3,	--12
+3,	--13
+3,	--14
+3,	--15
+3,	--16
+3,	--17
+3,	--18
+3,	--19
+3,	--20
+3,	--21
+3,	--22
+3,	--23
+3,	--24
+3,	--25
+3,	--26
+3,	--27
+3,	--28
+3,	--29
+3,	--30
+3,	--31
+3,	--32
+3,	--33
+3,	--34
+3,	--35
+3,	--36
+3,	--37
+}
+
 -- How large can the FWHM be? Larger lines are deleted. -1 skips FWHM check.
-max_line_widths={
--1,     --1
--1,     --2
--1,     --3
--1,     --4
--1,     --5
--1,     --6
--1,     --7
--1,     --8
--1,     --9
--1,     --10
--1,     --11
--1,     --12
--1,     --13
--1,     --14
--1,     --15
--1,     --16
--1,     --17
--1,     --18
--1,     --19
--1,     --20
--1}     --21
+-- After introducing max_line_gwidths, this variable is rudimentary since you
+-- can control funtions' gwidth directly. Write everything here -1.
+max_line_widths=
+{
+-1,	--1
+-1,	--2
+-1,	--3
+-1,	--4
+-1,	--5
+-1,	--6
+-1,	--7
+-1,	--8
+-1,	--9
+-1,	--10
+-1,	--11
+-1,	--12
+-1,	--13
+-1,	--14
+-1,	--15
+-1,	--16
+-1,	--17
+-1,	--18
+-1,	--19
+-1,	--20
+-1,	--21
+-1,	--22
+-1,	--23
+-1,	--24
+-1,	--25
+-1,	--26
+-1,	--27
+-1,	--28
+-1,	--29
+-1,	--30
+-1,	--31
+-1,	--32
+-1,	--33
+-1,	--34
+-1,	--35
+-1,	--36
+-1,	--37
+-1,     --38
+-1,     --39
+-1,     --40
+-1,     --41
+-1,     --42
+-- etc.
+}
+
 -- Binds gwidths to given maximum gwidth. If corresponding gwidth
 -- <=0 then doesn't bind line gwidth
-max_line_gwidths={
-50,     --1
-15,     --2
-15,     --3
-15,     --4
-40,     --5
-15,     --6
-15,     --7
-15,     --8
-15,     --9
-15,     --10
-15,     --11
-15,     --12
-15,     --13
-15,     --14
-15,     --15
-15,     --16
-15,     --17
-15,     --18
-40,     --19
-15,     --20
-15}     --21
+max_line_gwidths=
+{
+4,	--1
+4,	--2
+4,	--3
+4,	--4
+4,	--5
+4,	--6
+4,	--7
+4,	--8
+4,	--9
+4,	--10
+4,	--11
+4,	--12
+4,	--13
+5,	--14
+4,	--15
+5,	--16
+8,	--17
+4,	--18
+4,	--19
+4,	--20
+11,	--21
+4,	--22
+7,	--23
+4,	--24
+4,	--25
+5,	--26
+4,	--27
+5,	--28
+7,	--29
+4,	--30
+8,	--31
+8,	--32
+4,	--33
+6,	--34
+8,	--35
+4,	--36
+7,	--37
+}
+
 -- Where does the spectra actually start and end? (cutting away the edges) 
 start=445
-endpoint=1662
--- What are system paths for input folder and output folder
-input_path="/Users/jasper/Documents/Magistritöö/Koos/"
-output_path="/Users/jasper/Documents/Magistritöö/Output/"
+endpoint=1653
+
+-- What are system paths for input and output folder?
+-- Folders have to exist beforehand.
+input_path="/Users/jasper/repos/fityk-libs-spectra-script/Examples/387nm_Mo_example/Input/"
+output_path="/Users/jasper/repos/fityk-libs-spectra-script/Examples/387nm_Mo_example/Output/"
+
+-- Change this if you want to use multiple instances of Fityk calculating
+-- simultaneously using different inputs / different ranges. 
+input_info_name="info.txt"
+output_data_name="output.txt"
+
 -- What type of files do you use?
 file_end=".asc"
+
 -- When importing text into spreadsheet filename (e.g. 13.5) may be 
 -- read as a float. Using different separator (e.g. 13,5) avoids losing
 -- "decimal" zeros from the end of the value
 separator=","
+
 -- To plot or not to plot [true/false]?
 plot=true
+
 -- What are the plotting ranges? Use false to use automatic ranges
 -- e.g. x_min=false x_max=500 y_min=0.5 y_max=false
 x_min=start-(endpoint-start)/10
 x_max=endpoint+(endpoint-start)/10
 y_min=false
 y_max=false
--- If peak is considered wide then how wide is it?
+
+-- If peak is considered wide then how wide is it? -- somewhat rudimentary
 guess_initial_gwidth=25
--- Should the peaks be considered wide?
+
+-- Should the peaks be considered wide? -- somewhat rudimentary
 wide=false
+
 -- What is the minimal line gwidth? Functions with gwidt lower than here
 -- will be considered unphysical and written 0
-minimal_gwidth=0.3
+minimal_gwidth=0.5
+
+-- Do you want to stop for query for continuing after every file? [true/false]
+stop=false
 ----------------------------------------------------------------------
 -- Change constants above
 
@@ -170,6 +276,9 @@ center_error="-"
 center_errors={}
 gwidth_errors={}
 shape_errors={}
+file_check=nil
+experiment_check=nil
+stopscript=false
 
 ----------------------------------------------------------------------
 --Function declarations
@@ -190,7 +299,7 @@ end
 -- Saves info file into separate arrays so that @0 is empty.
 -- Loads info file, (columns: file nr;exposure time;nr of accumulations;gain;gate width)
 function load_info()
-  F:execute("@+ < "..input_path.."info.txt:1:2..::")
+  F:execute("@+ < "..input_path..input_info_name..":1:2..::")
   -- Loads data from info file
   -- Exposure times
   exposures_data=F:get_data(0)
@@ -225,7 +334,7 @@ end
 ------------------------------------------
 -- Initializes output file, change path if needed
 function init_output()
-  file=io.open(output_path.."output.txt","w")
+  file=io.open(output_path..output_data_name,"w")
   io.output(file)
   io.write("Experiment nr")
   io.write("\tCHI^2")
@@ -252,8 +361,13 @@ end
 ------------------------------------------
 -- Data initialization before looping over datasets
 function init_data1()
-  -- Loads 1 file. Change path if needed
-  F:execute("@+ <"..input_path..file_index..file_end..":1:2..::")
+  if experiment_check then
+    -- Loads 1 experiment from file
+    F:execute("@+ <"..input_path..file_index..file_end..":1:"..(experiment_check+1).."::")
+  else
+    -- Loads 1 file. Change path if needed
+    F:execute("@+ <"..input_path..file_index..file_end..":1:2..::")
+  end
   -- Finds nr. of series in file
   series_length=F:get_dataset_count()
 end
@@ -296,26 +410,27 @@ function guess_parameter_constructor(linenr)
     parameters=parameters..line_positions[linenr].."+"..
     line_center_domains[linenr].."*sin($center"..dataset_index.."_"..linenr..")"
   end
-  
   -- Shape
-  -- Angle variable
+  -- Angle variable (3pi/2)
   F:execute("$shape"..dataset_index.."_"..linenr.."=~0")
   -- shape=sin(~ąngle)
   parameters=parameters..",shape=0.5+0.5*sin($shape"..dataset_index.."_"..linenr..")"
   
   -- Gwidth
   if max_line_gwidths[linenr]>0 then
-    -- Angle variable
-    F:execute("$gwidth"..dataset_index.."_"..linenr.."=~0")
+    -- Angle variable starts from 3pi/2 so that sin is minimal
+    F:execute("$gwidth"..dataset_index.."_"..linenr.."=~4.712")
     -- If there's substantial line broadening, guess wider functions
     if wide then
-      -- gwidth=initial_gwidth+initial_gwidth*sin(~ąngle)
+      -- gwidth=initial_gwidth+(initial_gwidth-min_gwidth)/2*(sin(~ąngle)-1)
       parameters=parameters..",gwidth="..guess_initial_gwidth.."+"..
-      guess_initial_gwidth.."*sin($gwidth"..dataset_index.."_"..linenr..")"
+      ((guess_initial_gwidth-minimal_gwidth)/2).."*(sin($gwidth"..
+      dataset_index.."_"..linenr..")-1)"
     else
-      -- gwidth=1+max_width*sin(~ąngle)
-      parameters=parameters..",gwidth=0.5+"..
-      max_line_gwidths[linenr].."*sin($gwidth"..dataset_index.."_"..linenr..")"
+      -- gwidth=max_width+(max_width-min_width)/2*(sin(~ąngle)-1)
+      parameters=parameters..",gwidth="..max_line_gwidths[linenr].."+"..
+      ((max_line_gwidths[linenr]-minimal_gwidth)/2).."*(sin($gwidth"..
+      dataset_index.."_"..linenr..")-1)"
     end
   end
   
@@ -396,27 +511,27 @@ function get_errors()
     if functions[linenr]:get_param_value("height")>0 then
       -- Shape
       F:execute("$shape_error=$shape"..dataset_index.."_"..linenr..".error")
-      shape_errors[linenr]=math.cos(F:get_variable("shape"..dataset_index.."_"..linenr):value())
-      *F:get_variable("shape_error"):value()
+      shape_errors[linenr]=math.abs(0.5*math.cos(F:get_variable("shape"..dataset_index.."_"..linenr):value())
+      *F:get_variable("shape_error"):value())
       -- Center
       if line_center_domains[linenr]>0 then
         F:execute("$center_error=$center"..dataset_index.."_"..linenr..".error")
-        center_errors[linenr]=line_center_domains[linenr]*math.cos(
+        center_errors[linenr]=math.abs(line_center_domains[linenr]*math.cos(
         F:get_variable("center"..dataset_index.."_"..linenr):value())
-        *F:get_variable("center_error"):value()
+        *F:get_variable("center_error"):value())
       end
       -- Gwidth
       if max_line_gwidths[linenr]>0 then
         if wide then
           F:execute("$gwidth_error=$gwidth"..dataset_index.."_"..linenr..".error")
-          gwidth_errors[linenr]=guess_initial_gwidth*math.cos(
+          gwidth_errors[linenr]=math.abs((guess_initial_gwidth-minimal_gwidth)/2*math.cos(
           F:get_variable("gwidth"..dataset_index.."_"..linenr):value())
-          *F:get_variable("gwidth_error"):value()
+          *F:get_variable("gwidth_error"):value())
         else
           F:execute("$gwidth_error=$gwidth"..dataset_index.."_"..linenr..".error")
-          gwidth_errors[linenr]=max_line_gwidths[linenr]*math.cos(
+          gwidth_errors[linenr]=math.abs((max_line_gwidths[linenr]-minimal_gwidth)/2*math.cos(
           F:get_variable("gwidth"..dataset_index.."_"..linenr):value())
-          *F:get_variable("gwidth_error"):value()
+          *F:get_variable("gwidth_error"):value())
         end
       end
     end
@@ -425,7 +540,7 @@ end
 ------------------------------------------
 -- Writes parameters of the functions into output file
 function write_output()
-  file=io.open(output_path.."output.txt","a")
+  file=io.open(output_path..output_data_name,"a")
   io.output(file)
   -- Weighted sum of squared residuals, a.k.a. chi^2
   chi2=F:get_wssr(0)
@@ -546,18 +661,40 @@ if answer=='y' then
   init_output()
 end
 
+answer=F:input("Manually check 1 file? [y/n]")
+if answer=='y' then 
+  file_check=F:input("Number of file: ")
+  experiment_check=F:input("Experiment number in the series: ")
+end
+
 -- Iterates over files
 for n=first_filenr,last_filenr,1 do 
-  -- Globalizes the for loop index
-  file_index=n
+  -- Globalizes the for loop index and checks whether to view 1 file
+  if file_check then 
+    file_index=file_check
+  else
+    file_index=n
+  end
+  
   
   init_data1()
   
   -- Loops over datasets from file.
   for m=1,series_length,1 do
+    -- Check whether user wants to stop the script while it's still running
+    stopfile=io.open(input_path.."stopscript.txt","r")
+    io.input(stopfile)
+    content=io.read()
+    io.close(stopfile)
+    if content then
+      stopscript=true
+      print("Stopping the script since stopscript.txt isn't empty")
+      break
+    end
+    
     -- Globalizes the for loop index
     dataset_index=m
-    
+  
     init_data2()
     fit_functions()
     
@@ -566,18 +703,35 @@ for n=first_filenr,last_filenr,1 do
     
     check_functions()
     get_errors()
+    
+    -- if using 1 experiment view then writes dataset index as the number for output
+    if experiment_check then
+      dataset_index=experiment_check
+    end
+    
     write_output()  
     -- Plots current dataset with functions
     if plot then plot_functions() end
-    delete_dataset()
+    
+    if not experiment_check then
+      delete_dataset()
+    end
+    
     print("Experiment: "..file_index..separator..dataset_index.." done.")
   end
   
-  -- Stop at current file for debugging
-  answer=F:input("Stop at file "..file_index.."? [y/n]")
-  if answer=='y' then 
+  -- Stop the loop if using 1 experiment view or user wants to stop the script
+  if file_check or stopscript then
     break
   end
-
+  
+  -- Stop at current file for debugging
+  if stop then
+    answer=F:input("Stop at file "..file_index.."? [y/n]")
+    if answer=='y' then 
+      break
+    end
+  end
+  
   print("File nr "..file_index.." done.")
 end
